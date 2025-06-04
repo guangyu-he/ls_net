634967035b1156ee4e9e40a46437e3ec080e4e41
Guangyu He <guangyu.he@golden-tech.de>
2025-06-04 14:15:27 +0200

    feat(cli): add option to display only main IP address

--- src/main.rs
+++ src/main.rs
@@ -13,16 +13,36 @@ struct Args {
     /// Protocol type to use: "all", "ipv4", or "ipv6". Defaults to "ipv4".
     #[clap(short, long, default_value="ipv4", value_parser=["all","ipv4","ipv6"])]
     protocol: String,
+
+    /// Only show the main IP address of the machine
+    #[clap(long)]
+    ip: bool,
 }
 
-fn run(protocol: &str) -> Result<()> {
+fn run(protocol: &str, only_show_ip: bool) -> Result<()> {
+    let only_show_ip = only_show_ip;
+
+    let main_ip_addr = machine_main_ip::get_local_ip().unwrap_or_else(|e| {
+        eprintln!("Error getting IP address: {}", e);
+        std::process::exit(1);
+    });
+
+    if only_show_ip {
+        println!("{}", main_ip_addr);
+        return Ok(());
+    }
+
     println!(
         "{}",
         "Local Network Interfaces and IP Addresses".green().bold()
     );
     match machine_main_ip::get_local_ip() {
         Ok(ip) => {
-            println!("{} {}", "Main IP address: ".blue(), ip.to_string().yellow());
+            println!(
+                "{} {}",
+                "Main IP address: ".blue().bold(),
+                ip.to_string().yellow()
+            );
         }
         Err(e) => eprintln!("Error getting IP address: {}", e),
     };
@@ -48,9 +68,7 @@ fn run(protocol: &str) -> Result<()> {
 
 fn main() -> Result<()> {
     let args = Args::parse();
-    let protocol = args.protocol.to_lowercase();
-
-    run(&protocol)
+    run(&args.protocol, args.ip)
 }
 
 #[cfg(test)]
@@ -59,19 +77,19 @@ mod tests {
 
     #[test]
     fn run_v4() {
-        let result = run("ipv4");
+        let result = run("ipv4", false);
         assert!(result.is_ok());
     }
 
     #[test]
     fn run_v6() {
-        let result = run("ipv6");
+        let result = run("ipv6", false);
         assert!(result.is_ok());
     }
 
     #[test]
     fn run_all() {
-        let result = run("all");
+        let result = run("all", false);
         assert!(result.is_ok());
     }
 }
